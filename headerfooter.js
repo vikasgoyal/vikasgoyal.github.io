a589799a81a93633b194004a042a6078ff281966
Vikas Goyal <61876549+vikasgoyal@users.noreply.github.com>
2024-07-07 19:06:00 -0500

    Add files via upload

--- headerfooter.js
+++ headerfooter.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('header.html')
+    fetch('https://vikasgoyal.github.io/header.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('header').innerHTML = data;
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
   .catch(error => console.error('Error loading the header:', error)); 
   });
   document.addEventListener("DOMContentLoaded", function() {
-    fetch('footer.html')
+    fetch('https://vikasgoyal.github.io/footer.html')
       .then(response => response.text())
       .then(data => {
         document.getElementById('footer').innerHTML = data;
